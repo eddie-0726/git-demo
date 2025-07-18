a194f5519c33b30e4fed7898d6dfc0b63ed7da17
badappu <badappu0723@gmail.com>
2025-07-18 20:22:04 +0800

    增加風險處理

--- 3.py
+++ 3.py
@@ -1,1 +1,19 @@
-111
+
+while True:
+    try:   
+        a_input = (input("請輸入a:"))
+        if a_input== "exit":
+            break
+        a = eval(a_input)
+        b = eval(input("請輸入b:"))
+
+        if a == b :
+            print("ab一樣大")
+        elif a > b:
+            print(f"a>b ,相差:{a-b}")
+        else:
+            print(f"b>a ,相差:{b-a}")
+
+    except Exception as e :
+        print(f"發生錯誤，{e}")
+
